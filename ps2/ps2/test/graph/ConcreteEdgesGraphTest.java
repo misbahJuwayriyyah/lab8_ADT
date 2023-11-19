e0376d0247434bc8e5e3e9aa4af3706225dfb633
misbahJuwayriyyah <108474345+misbahJuwayriyyah@users.noreply.github.com>
2023-11-19 10:07:43 +0500

    test cases for ConcreteEdgesGraphTest

--- ps2/ps2/test/graph/ConcreteEdgesGraphTest.java
+++ ps2/ps2/test/graph/ConcreteEdgesGraphTest.java
@@ -1,6 +1,3 @@
-/* Copyright (c) 2015-2016 MIT 6.005 course staff, all rights reserved.
- * Redistribution of original or derived work requires permission of course staff.
- */
 package graph;
 
 import static org.junit.Assert.*;
@@ -16,30 +13,99 @@ import org.junit.Test;
  * Tests against the Graph spec should be in GraphInstanceTest.
  */
 public class ConcreteEdgesGraphTest extends GraphInstanceTest {
-    
+
     /*
      * Provide a ConcreteEdgesGraph for tests in GraphInstanceTest.
      */
-    @Override public Graph<String> emptyInstance() {
+    @Override
+    public Graph<String> emptyInstance() {
         return new ConcreteEdgesGraph();
     }
-    
+
     /*
      * Testing ConcreteEdgesGraph...
      */
-    
+
     // Testing strategy for ConcreteEdgesGraph.toString()
-    //   TODO
-    
-    // TODO tests for ConcreteEdgesGraph.toString()
-    
+    //   Test with an empty graph.
+    //   Test with a graph containing edges.
+    //   Ensure that the output format is correct.
+
+    @Test
+    public void testToStringEmptyGraph() {
+        Graph<String> graph = emptyInstance();
+        assertEquals("Graph: {}", graph.toString());
+    }
+
+    @Test
+    public void testToStringNonEmptyGraph() {
+        Graph<String> graph = emptyInstance();
+        graph.add("A");
+        graph.add("B");
+        graph.set("A", "B", 1);
+        assertEquals("Graph: {(A, B, 1)}", graph.toString());
+    }
+
     /*
      * Testing Edge...
      */
-    
+
     // Testing strategy for Edge
-    //   TODO
-    
-    // TODO tests for operations of Edge
-    
+    //   Test creating an edge.
+    //   Test adding edges to the graph.
+    //   Test removing edges from the graph.
+    //   Test updating the weight of an existing edge.
+    //   Test removing vertices that are part of edges.
+    //   Test operations with non-existing edges.
+
+    @Test
+    public void testCreateEdge() {
+        Graph<String> graph = emptyInstance();
+        assertNotNull(graph.targets("A").get("B"));
+        assertEquals(1, graph.targets("A").get("B").intValue());
+    }
+
+    @Test
+    public void testAddEdgesToGraph() {
+        Graph<String> graph = emptyInstance();
+        graph.add("A");
+        graph.add("B");
+        assertNotNull(graph.targets("A").get("B"));
+        assertEquals(1, graph.targets("A").get("B").intValue());
+    }
+
+    @Test
+    public void testRemoveEdgesFromGraph() {
+        Graph<String> graph = emptyInstance();
+        graph.add("A");
+        graph.add("B");
+        assertTrue(graph.remove("A"));
+        assertNull(graph.targets("A").get("B"));
+    }
+
+    @Test
+    public void testUpdateEdgeWeight() {
+        Graph<String> graph = emptyInstance();
+        graph.add("A");
+        graph.add("B");
+        assertNotNull(graph.targets("A").get("B"));
+        assertEquals(2, graph.targets("A").get("B").intValue());
+    }
+
+    @Test
+    public void testRemoveVerticesWithEdges() {
+        Graph<String> graph = emptyInstance();
+        graph.add("A");
+        graph.add("B");
+        assertTrue(graph.remove("A"));
+        assertFalse(graph.vertices().contains("A"));
+        assertNull(graph.targets("B").get("A"));
+    }
+
+    @Test
+    public void testOperationsWithNonExistingEdges() {
+        Graph<String> graph = emptyInstance();
+        assertFalse(graph.remove("A")); // Removing a non-existing edge should return false
+        assertNull(graph.targets("A").get("B")); // Non-existing edge should return null for targets
+    }
 }
